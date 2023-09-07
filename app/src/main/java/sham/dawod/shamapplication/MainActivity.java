6f66a12770ad1ff988746b7099b69518b5986d12
Owner <alsalamcse>
2023-09-07 14:00:23 +0300

    akjl;aed;jolae'r'teysj

--- app/src/main/java/sham/dawod/shamapplication/MainActivity.java
+++ app/src/main/java/sham/dawod/shamapplication/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        //456456456465
         setContentView(R.layout.activity_main);
     }
 }
