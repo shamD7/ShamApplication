1c86bb4c30a7ac88420ce945fde995433042b727
Owner <alsalamcse>
2023-09-14 15:00:01 +0300

    akjl;aed;jolae'r'teysj

--- app/src/main/java/sham/dawod/shamapplication/MainActivity.java
+++ app/src/main/java/sham/dawod/shamapplication/MainActivity.java
@@ -2,6 +2,8 @@ package sham.dawod.shamapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +14,31 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         //456456456465
         setContentView(R.layout.activity_main);
+        Log.d("SD","onCreate" );
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d("SD","onRestart" );
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d("SD","onResume" );
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("SD","onStop" );
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
     }
 }
