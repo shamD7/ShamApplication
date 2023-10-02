5b3882dabd44bcebcebb5c33a22e5911a4248b8e
Owner <alsalamcse>
2023-10-02 14:13:14 +0300

    تسالي احلا عالم

--- app/src/main/java/sham/dawod/shamapplication/SighUpActivity.java
+++ app/src/main/java/sham/dawod/shamapplication/SighUpActivity.java
@@ -2,12 +2,31 @@ package sham.dawod.shamapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class SighUpActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+public class SighUpActivity extends AppCompatActivity
+{
+    private TextInputEditText etEmail;
+    private TextInputEditText etPassword;
+    private TextInputEditText etRepassword;
+    private TextInputEditText etName;
+    private TextInputEditText etPhone;
+    private Button btnSAVE;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_sigh_up);
+        etEmail=(TextInputEditText) findViewById(R.id.etEmail);
+        etPassword=(TextInputEditText)findViewById(R.id.etPassword);
+        etRepassword=(TextInputEditText)findViewById(R.id.etRepassword);
+        etName=(TextInputEditText)findViewById(R.id.etName);
+        etPhone=(TextInputEditText)findViewById(R.id.etPhone);
+        btnSAVE=(Button) findViewById(R.id.btnSAVE);
+
     }
 }
--- app/src/main/java/sham/dawod/shamapplication/SignInActivity.java
+++ app/src/main/java/sham/dawod/shamapplication/SignInActivity.java
@@ -1,13 +1,27 @@
 package sham.dawod.shamapplication;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
-public class SignInActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+
+public class SighInActivity extends AppCompatActivity
+{
+    private TextInputEditText etEmail;
+    private TextInputEditText etPassword;
+    private Button btnSighIn;
+    private Button btnSighUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_sigh_up);
+        setContentView(R.layout.activity_sigh_in);
+        etEmail=(TextInputEditText) findViewById(R.id.etEmail);
+        etPassword=(TextInputEditText)findViewById(R.id.etPassword);
+        btnSighIn=(Button) findViewById(R.id.btnSignIn);
+        btnSighUp=(Button) findViewById(R.id.btnSignUp);
+
     }
 }
